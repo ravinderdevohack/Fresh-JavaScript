920d93909ea7bdc5ae391196a847499e0f6ab2b8
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-04 10:52:12 +0000

    Stack and heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -14,9 +14,9 @@ let outsideTemp = null;
 let id = Symbol('123')
 let anotherId = Symbol('123')
 
-console.log(id === anotherId);
-console.log(id);
-console.log(typeof id);
+// console.log(id === anotherId);
+// console.log(id);
+// console.log(typeof id);
 
 // const bigNumber = 234343n;
 
@@ -35,7 +35,7 @@ let firstFunction = function(){
     console.log("hello world");
 }
 
-firstFunction()
+// firstFunction()
 
 // JS Object and Events(web and browser) main thing in JS
 
@@ -44,4 +44,32 @@ firstFunction()
     but in JS we don't need to define the const or let so it's a dynamic type language
     JavaScript allows changing the type of variable anytime → dynamic typing.
 */
+
+// ===================================================================================
+
+
+// Stack (Primitive Datatype) and Heap (Non- Primitive Datatype) memory type
+
+//Stack  memory type example
+let firstUserName = "ravinder"
+let anotherUserName = firstFunction;
+
+anotherUserName = "kissan"
+
+console.log(firstUserName);
+console.log(anotherUserName);
+
+
+//Heap memory type example
+let userOne = {
+    email: "ravinder@google.com",
+    upi: "233243@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "kissan@yahoo.com"
+
+console.log(userOne);
+console.log(userTwo);
 
