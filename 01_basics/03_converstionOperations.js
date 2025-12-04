d141b1a79ad36af6ef720b6b0613358f55b4852a
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-04 06:45:14 +0000

    Conversion of Datatype

---
+++ 01_basics/03_converstionOperations.js
@@ -0,0 +1,24 @@
+let score = "23esf"
+
+// console.log(typeof score)
+
+let convertToNumber = Number(score)
+
+// console.log(typeof (convertToNumber));
+// console.log(convertToNumber);
+
+
+// "33" => 33
+// "33asdf" => NaN -> not a number
+// null => NaN
+// undefiend => undefiend
+// true/false => 1/0
+
+let isLoggedIn = "ravi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; // 0 => false
+// "" => false ; 
+// "ravi" => true
