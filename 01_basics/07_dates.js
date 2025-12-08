6b956939636dedcc110e8244574e2853ddf37cfc
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-08 09:57:50 +0000

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+
+let myDate = new Date()
+
+// console.log(typeof myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2023, 0, 23);
+let myCreatedDate = new Date("2023-01-23")
+console.log(myCreatedDate.toLocaleString())
