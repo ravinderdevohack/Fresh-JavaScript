539206c5e1c3eb447148c10d4aa438b901a34373
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-11 06:32:10 +0000

    reduce proto

--- 04_iteration/01_loop.js
+++ 04_iteration/01_loop.js
@@ -1,7 +1,10 @@
 //  for of   -> can be work on array but on no object , cause object is non-iterable
 //  for in   -> can wokr on object
-//  foreach  -> will only work for array, as its a proto of arry return a call back function, call back function doesn't have a name
-//  Map -> give latest unqiue value if duplicate is found
+//  foreach  -> will only work for array, as its a proto of arry, a call back function, call back function doesn't have a name
+//  filter   -> same as foreach proto of array, difference is its return value but for each not
+//  map      -> call back function
+
+//  Map -> give latest unqiue value if duplicate is found  , 
 
 const arr = [1, 2, 3, 4, 5]
 
@@ -60,3 +63,42 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 
 // myObject.forEach( (item) => {console.log(item.key);
 // })
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const filterValue = myNums.filter( (item) => item > 5 )    // implicit return
+// console.log(filterValue);
+
+const filterValue2 = myNums.filter( (item) => { return item < 5} )    // explicit return
+// console.log(filterValue2);
+// console.log(typeof(filterValue));
+
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+let userBook = books.filter( (item) => item['genre'] == 'Fiction')
+console.log(userBook);
+
+
+
+// Chaining
+
+let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let newNums = myNumbers
+                    .map( (iteam) => iteam*10 )
+                    .map( (iteam) => iteam +1 )
+                    .filter( (iteam) => iteam > 40)
+
+console.log(newNums);
