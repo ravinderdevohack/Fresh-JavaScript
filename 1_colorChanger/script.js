38ecd714d42121268fb4731e15abb37601210beb
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-15 11:20:34 +0000

    background color changer

---
+++ 1_colorChanger/script.js
@@ -0,0 +1,31 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+
+// console.log(buttons);
+
+buttons.forEach(function(button) {
+    console.log(button);
+    button.addEventListener('click', function(e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id == 'grey') { 
+            // console.log(3); 
+            body.style.backgroundColor = e.target.id; 
+        }
+        if(e.target.id == 'blue') { 
+            body.style.backgroundColor = e.target.id; 
+        }
+        if(e.target.id == 'white') { 
+            body.style.backgroundColor = e.target.id; 
+        }
+        if(e.target.id == 'yellow') { 
+            body.style.backgroundColor = e.target.id; 
+        }
+        if(e.target.id == 'purple') { 
+            body.style.backgroundColor = e.target.id; 
+        }
+        
+    })
+
+    
+})
